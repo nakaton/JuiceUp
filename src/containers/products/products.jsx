245a91279b672cef3c214caa9e027c4e46be9f21
Nick <yangyongdun@126.com>
2020-04-21 10:27:35 +1200

    change product img

--- src/containers/products/products.jsx
+++ src/containers/products/products.jsx
@@ -2,26 +2,27 @@ import React, { Component } from "react";
 import ProductItem from "../../components/product-item/product-item";
 import "./products.css";
 
-import avocado from "../../assets/category/avocado.svg";
-import mango from "../../assets/category/mango.svg";
-import grapes from "../../assets/category/grapes.svg";
-import apple from "../../assets/category/apple.svg";
-import orange from "../../assets/category/orange.svg";
-import pear from "../../assets/category/pear.svg";
+// import avocado from "../../assets/category/avocado.svg";
+// import mango from "../../assets/category/mango.svg";
+// import grapes from "../../assets/category/grapes.svg";
+// import apple from "../../assets/category/apple.svg";
+// import orange from "../../assets/category/orange.svg";
+// import pear from "../../assets/category/pear.svg";
+
+import avocado from "../../assets/product/wine/celebratory-cherry.png";
+import mango from "../../assets/product/wine/celebratory_glogg.png";
+import grapes from "../../assets/product/wine/kosher-grape.png";
+import apple from "../../assets/product/wine/celebratory-caramel-apple.png";
+import orange from "../../assets/product/wine/celebratory-cranberry.png";
+import pear from "../../assets/product/wine/celebratory-organic-pear.png";
 
 class Products extends Component {
   state = {
     productItems: [
       {
-        imgUrl: grapes,
-        name: "Grapes Juice",
-        color: "#ECE2FA",
-        price: 2.50
-      },
-      {
         imgUrl: apple,
         name: "Apple Juice",
-        color: "#FFE2E0",
+        color: "#FA818A",
         price: 3.50
       },
       {
@@ -31,21 +32,27 @@ class Products extends Component {
         price: 9.50
       },
       {
+        imgUrl: grapes,
+        name: "Grapes Juice",
+        color: "#D5AECD",
+        price: 2.50
+      },
+      {
         imgUrl: mango,
         name: "Mango Juice",
-        color: "#FFF2E1",
+        color: "#FEE0B9",
         price: 10.5
       },
       {
         imgUrl: avocado,
         name: "Avocado Juice",
-        color: "#E0F0E4",
+        color: "#ACDAB4",
         price: 8.65
       },
       {
         imgUrl: orange,
         name: "Orange Juice",
-        color: "#FFEEDB",
+        color: "#FCD855",
         price: 5.50
       },
     ],
